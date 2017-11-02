36a1702ab3f465d690fc1b2fbd254e123d778f06
arash parnia <arashparnia@gmail.com>
2017-11-02 13:04:14 +0100

    model building

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from pandas import scatter_matrix
 from pandas.io.parsers import read_csv
-# from sklearn import preprocessing, model_selection
+from sklearn import preprocessing, model_selection
 # from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 # from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
@@ -10,6 +10,7 @@ from sklearn.neural_network import MLPClassifier, MLPRegressor
 from sklearn.metrics import accuracy_score
 import numpy as np
 import pandas as pd
+
 
 from sklearn.externals import joblib
 
@@ -27,12 +28,26 @@ train_X, test_X, train_y, test_y = train_test_split(X,Y,test_size= 0.3,random_st
 print(list(train_y))
 print(list(test_X))
 
-clf = MLPRegressor(solver='lbfgs', alpha=1e-5, hidden_layer_sizes=(5, 2), random_state=1,verbose = True)
+clf = MLPRegressor(solver='lbfgs', alpha=1e-5, hidden_layer_sizes=(200, 100), random_state=42,verbose = True)
 clf.fit(train_X,train_y )
 
 
 pred_y =  clf.predict(test_X)
 
+pred_y = pd.DataFrame(pred_y)
+# print(pred_y.head())
+# print(test_y.head())
+
 # print(accuracy_score(test_y,pred_y,normalize=False))
+kfold = model_selection.KFold(n_splits=10,random_state=42)
+scoring = "neg_mean_absolute_error"
+results = model_selection.cross_val_score(clf,X,Y,cv=kfold,scoring=scoring)
+print("-----------------------------")
+print(results.mean(),results.std())
+
 
 joblib.dump(clf, 'model.pkl')
+
+# load with this
+# clf = joblib.load('filename.pkl')
+#   commands to car = clf.predict(input from car sensors)
