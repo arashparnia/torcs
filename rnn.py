b4d34d53cdb8b38a13fdfb31f20385e778d294c0
arash parnia <arashparnia@gmail.com>
2017-11-09 19:38:01 +0100

    RNN new

--- rnn.py
+++ rnn.py
@@ -60,9 +60,10 @@ data = pd.concat([data3,data2,data1])
 data  = data.fillna(data.interpolate(),axis=0,inplace=False)
 d1 = copy.deepcopy(data)
 d2 = copy.deepcopy(data)
-Y = pd.DataFrame(d1[['ACCELERATION','BRAKE','STEERING']])
-X = pd.DataFrame(d2[['SPEED', 'TRACK_POSITION', 'ANGLE_TO_TRACK_AXIS', 'TRACK_EDGE_0', 'TRACK_EDGE_1', 'TRACK_EDGE_2', 'TRACK_EDGE_3', 'TRACK_EDGE_4', 'TRACK_EDGE_5', 'TRACK_EDGE_6', 'TRACK_EDGE_7', 'TRACK_EDGE_8', 'TRACK_EDGE_9', 'TRACK_EDGE_10', 'TRACK_EDGE_11', 'TRACK_EDGE_12', 'TRACK_EDGE_13', 'TRACK_EDGE_14', 'TRACK_EDGE_15', 'TRACK_EDGE_16', 'TRACK_EDGE_17', 'TRACK_EDGE_18']])
-
+# Y = pd.DataFrame(d1[['ACCELERATION','BRAKE','STEERING']])
+Y = pd.DataFrame(d1[['STEERING']])
+X = pd.DataFrame(d2[['TRACK_POSITION', 'ANGLE_TO_TRACK_AXIS']])
+# X = pd.DataFrame(d2[['SPEED', 'TRACK_POSITION', 'ANGLE_TO_TRACK_AXIS', 'TRACK_EDGE_0', 'TRACK_EDGE_1', 'TRACK_EDGE_2', 'TRACK_EDGE_3', 'TRACK_EDGE_4', 'TRACK_EDGE_5', 'TRACK_EDGE_6', 'TRACK_EDGE_7', 'TRACK_EDGE_8', 'TRACK_EDGE_9', 'TRACK_EDGE_10', 'TRACK_EDGE_11', 'TRACK_EDGE_12', 'TRACK_EDGE_13', 'TRACK_EDGE_14', 'TRACK_EDGE_15', 'TRACK_EDGE_16', 'TRACK_EDGE_17', 'TRACK_EDGE_18']])
 
 
 # X = X.values.tolist()
@@ -72,6 +73,51 @@ X_train, X_test, Y_train, Y_test = train_test_split(X,Y,test_size= 0.2,random_st
 
 
 rng = np.random.RandomState(42)
+
+def frequency_generator(N,min_period,max_period,n_changepoints):
+    """returns a random step function with N changepoints
+       and a sine wave signal that changes its frequency at
+       each such step, in the limits given by min_ and max_period."""
+    # vector of random indices < N, padded with 0 and N at the ends:
+    changepoints = np.insert(np.sort(rng.randint(0,N,n_changepoints)),[0,n_changepoints],[0,N])
+    # list of interval boundaries between which the control sequence should be constant:
+    const_intervals = list(zip(changepoints,np.roll(changepoints,-1)))[:-1]
+    # populate a control sequence
+    frequency_control = np.zeros((N,1))
+    for (t0,t1) in const_intervals:
+        frequency_control[t0:t1] = rng.rand()
+    periods = frequency_control * (max_period - min_period) + max_period
+    # run time through a sine, while changing the period length
+    frequency_output = np.zeros((N,1))
+    z = 0
+    for i in range(N):
+        z = z + 2 * np.pi / periods[i]
+        frequency_output[i] = (np.sin(z) + 1)/2
+    return np.hstack([np.ones((N,1)),1-frequency_control]),frequency_output
+
+
+N = 15000 # signal length
+min_period = 2
+max_period = 10
+n_changepoints = int(N/200)
+frequency_control,frequency_output = frequency_generator(N,min_period,max_period,n_changepoints)
+
+traintest_cutoff = int(np.ceil(0.7*N))
+
+train_ctrl,train_output = frequency_control[:traintest_cutoff],frequency_output[:traintest_cutoff]
+test_ctrl, test_output  = frequency_control[traintest_cutoff:],frequency_output[traintest_cutoff:]
+
+print(type(train_ctrl))
+# print(train_output)
+
+
+# os._exit()
+rng = np.random.RandomState(42)
+print(X_train.shape)
+print(Y_train.shape)
+
+X_train = np.array(X_train)
+Y_train = np.array(Y_train)
 
 print(ESN)
 esn = ESN.ESN(n_inputs = 2,
