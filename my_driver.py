3f259d085924dcbc12bcdad2be42887069c68f16
arash parnia <arashparnia@gmail.com>
2017-11-26 19:46:05 +0100

    neat steering output only

--- my_driver.py
+++ my_driver.py
@@ -13,6 +13,7 @@ from sklearn.preprocessing import LabelEncoder, MinMaxScaler
 class MyDriver(Driver):
 
     reward = 0
+    punishment = 0
     # Override the `drive` method to create your own driver
     ...
 
@@ -28,6 +29,8 @@ class MyDriver(Driver):
         distance_from_edge = list(carstate.distances_from_edge[0:19])
         opponents = list(carstate.opponents[0:36])
         data = [carstate.speed_x,carstate.speed_y, carstate.distance_from_center, carstate.angle] + distance_from_edge + opponents
+        # data = [carstate.speed_x, carstate.distance_from_center,
+        #         carstate.angle] + distance_from_edge
         data = pd.DataFrame(data)
         data = np.array(data)
 ##########################################################
@@ -36,25 +39,27 @@ class MyDriver(Driver):
         with open('network.p', 'rb') as input:
             net = pickle.load(input)
         data = net.activate(data)
-        command.accelerator = data[0]
-        command.brake = data[1]
-        command.steering = data[2]
+        # command.accelerator = data[0]
+        # command.brake = data[1]
+        # command.steering = data[2]
+        if (carstate.speed_x < 60):
+            command.accelerator = 0.5
+        command.brake = 0
+        command.steering = data[0]
 ##########################################################
 
-        MAX_COUNTER = 30
+        self.reward +=  1 - abs(carstate.distance_from_center)
+
+        MAX_COUNTER = 60
 
         # self.counter = self.counter + 1
 
         fitness = carstate.distance_from_start
-        if fitness > 2000:
+        fitness += self.reward
+
+        if fitness > 22000:
             fitness =0
 
-        if command.accelerator > 0.9:
-            fitness += 1
-        if command.brake > 0.1:
-            fitness -= 1
-        if abs(command.steering) > 0.1:
-            fitness -= 1
         # self.reward +=  ( 180  - (abs(carstate.angle))) * 0.001
 
         # fitness += self.reward
@@ -65,27 +70,29 @@ class MyDriver(Driver):
             print("COUNTER MAX    fitness: ", fitness, " counter: ", counter, " Distance: ",carstate.distance_from_start)
             command.meta = 1
 
-        # if (counter > 5 and command.accelerator < 0.7):
-        #     fitness = 0
-        #     self.fitness_to_file(fitness)
-        #     print("DID NOT ACCELERATE   fitness: ", fitness, " counter: ", counter, " Distance: ",carstate.distance_from_start)
-        #     command.meta = 1
+        if (counter > 5 and counter < 20 and carstate.speed_x < 4):
+            fitness = 0
+            self.fitness_to_file(fitness)
+            print("DID NOT MOVE   fitness: ", fitness, " counter: ", counter, " Distance: ",carstate.distance_from_start)
+            command.meta = 1
 
-        # if (carstate.distance_raced < -5 ):
-        #     fitness = 0
-        #     self.fitness_to_file(fitness)
-        #     print("BACKEWARD      fitness: ", fitness , " counter: ", counter, " Distance: ",carstate.distance_from_start)
-        #     command.meta = 1
+        if (carstate.distance_raced < -2 ):
+            fitness = 0
+            self.fitness_to_file(fitness)
+            print("BACKWARD fitness: ", fitness , " counter: ", counter, " Distance: ",carstate.distance_from_start)
+            command.meta = 1
 
-        # if ( abs(carstate.distance_from_center) > 0.9 ):
-        #     self.fitness_to_file(fitness)
-        #     print("OUT OF TRACK   fitness: ", fitness, " counter: ", counter, " Distance: ",carstate.distance_from_start)
-        #     command.meta = 1
+        if ( abs(carstate.distance_from_center) > 1.1 ):
+            print(carstate.distance_raced)
+            print(carstate.distance_from_start)
+            self.fitness_to_file(fitness)
+            print("OUT OF TRACK   fitness: ", fitness, " counter: ", counter, " Distance: ",carstate.distance_from_start)
+            command.meta = 1
 
-        # if (carstate.damage > 5000):
-        #     self.fitness_to_file(fitness)
-        #     print("DAMAGED        fitness: ", fitness, " counter: ", counter, " Distance: ",carstate.distance_from_start)
-        #     command.meta = 1
+        if (carstate.damage > 5000):
+            self.fitness_to_file(fitness)
+            print("DAMAGED        fitness: ", fitness, " counter: ", counter, " Distance: ",carstate.distance_from_start)
+            command.meta = 1
 ##########################################################
         # print("Min opponent distance is", map(min,(zip(*carstate.opponents))))
         # print("list of tuple is: ", (carstate.opponents)[0])
